1c45d219c67fd461f1d3ccbfe7ded072bc9c09fa
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2017-12-14 11:38:11 -0500

    pushing yesterdays final changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,32 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find_by_id(params[:id])
   end
 
-  def new
-  end
-
-  def create
-  end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Updated your information'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :bio, :location, :age, :height, :weight)
+  end 
 end
