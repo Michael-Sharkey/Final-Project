27819041b79012a4dc099420f1af50cf38a57e61
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2017-12-14 11:45:19 -0500

    establish training session, movement, muscle group, and muscle associations

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,2 +1,5 @@
 class Movement < ApplicationRecord
+  has_and_belongs_to_many :musclegroups
+  has_many :muscles, through: :musclegroups
+  has_and_belongs_to_many :training_sessions
 end
--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -1,2 +1,8 @@
 class TrainingSession < ApplicationRecord
+  belongs_to :user
+  has_many :worksets
+  has_many :musclegroups
+  has_many :muscles, through: :musclegroups
+  has_and_belongs_to_many :movements
+
 end
