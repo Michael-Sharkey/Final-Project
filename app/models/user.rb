8032c0d3f2a31a2b47f5fea5d6676d3e93f3edab
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2017-12-15 13:53:37 -0500

    update model and create new migration for user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # gem dependencies
+  # devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # paperclip
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   has_many :training_sessions
   has_many :worksets
