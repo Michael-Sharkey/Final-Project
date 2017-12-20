67d3ac47e57ffde495541aaaa595c8ba2eabd2d4
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2017-12-20 14:38:54 -0500

    small fixes

--- app/assets/javascripts/training_sessions.js
+++ app/assets/javascripts/training_sessions.js
@@ -6,11 +6,13 @@ $(document).ready(function() {
   function pickFilter() {
     let mPSelected = mP.selectedOptions[0].innerText;
     console.log(mPSelected);
+    // save options from select field to array before deleting
     let exercises = [];
     for (let i = 1; i < eX.children.length; i++) {
       exercises.push(eX.children[i]);
     };
     eX.innerHTML = '';
+    // Use 'for each' instead of for loop to avoid lazy loading, and apply function to all children
     exercises.forEach(function(x) {
       if(x.label === mPSelected) {
         console.log(x.innerHTML);
