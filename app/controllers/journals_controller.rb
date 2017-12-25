65071967f9515f724b07f89a3b97f6149cb28428
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2017-12-25 12:51:26 -0500

    create new journal entry form

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -6,13 +6,22 @@ class JournalsController < ApplicationController
   end
 
   def new
+    @journal = Journal.new
   end
 
   def create
+    @journal = Journal.new(journal_params)
+    @journal.user_id = current_user.id
+    if @journal.save
+      redirect_to @journal, notice: 'Success!'
+    else
+      render 'new', notice: 'Something Went Wrong'
+    end
   end
 
   private
 
   def journal_params
+    params.require(:journal).permit(:id, :user_id, :title, :entry)
   end
 end
