60569ce60c989446bc70c838bf0ec22d64ac8807
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-01 11:35:32 -0500

    reseed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225180309) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "journal_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["journal_id"], name: "index_comments_on_journal_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 20171225174322) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
