bb4259d6e9edbb19476dadb538584c7f2584877c
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-02 08:07:42 -0500

    little fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,19 @@
 class UsersController < ApplicationController
+  before_action :get_user
+
   def index
   end
 
   def show
-    get_user
     @active = @user.meals.last
     @meals = @user.meals.order(created_at: :desc).offset(1)
   end
 
 
   def edit
-    @user = current_user
   end
 
   def update
-    @user = current_user
     if @user.update(user_params)
       redirect_to user_path(@user), notice: 'Updated your information'
     else
