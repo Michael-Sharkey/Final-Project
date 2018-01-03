66a788adbd8daa8a41a5725106c58ddeab2178af
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-02 21:45:16 -0500

    fixed models yet again to account for form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def show
     @active = @user.meals.last
     @meals = @user.meals.order(created_at: :desc).offset(1)
+    
   end
 
 
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,9 @@
 class Workout < ApplicationRecord
   belongs_to :user, optional: true
   has_many :worksets, inverse_of: :workout, dependent: :destroy
+
+
   accepts_nested_attributes_for :worksets, allow_destroy: true
+
+
 end
