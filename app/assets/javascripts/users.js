16b5638b774684a25852600dad99c1d36fd40b71
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-08 14:49:05 -0500

    add basic side nav hidden functionality

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
 
+  const navButton = document.getElementById('nav-button');
+  const closeNvg = document.getElementById('close-nav');
+
+  function openNav() {
+      document.getElementById("sideNav").style.width = "250px";
+      document.getElementById("mainArea").style.marginLeft = "250px";
+  };
+
+  function closeNav() {
+      document.getElementById("sideNav").style.width = "0";
+      document.getElementById("mainArea").style.marginLeft = "0";
+  };
+
+  navButton.addEventListener('click', openNav);
+  closeNvg.addEventListener('click', closeNav);
+
+
+});
