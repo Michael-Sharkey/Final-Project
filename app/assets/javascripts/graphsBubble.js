9766fdca693a4572db4b910a94239cf94dbc244c
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-09 09:16:33 -0500

    add toggle to sideNav button

--- app/assets/javascripts/graphsBubble.js
+++ app/assets/javascripts/graphsBubble.js
@@ -1,57 +1,57 @@
-$(document).ready(function(){
-
-  $.ajax({
-    type: 'GET',
-    contentType: 'application/json; charset=utf-8',
-    url: '../graphs/bubble',
-    dataType: 'json',
-    success: function(data){
-      drawBubble(data);
-      },
-    failure: function(result){
-      error();
-      }
-  });
-
-function error() {
-    console.log("Something went wrong!");
-}
-
-function drawBubble(data) {
-
-  var ctx = document.getElementById("bubbleGraph");
-  console.log(data);
-
-
-  var bubble = new Chart(ctx, {
-      type: 'bubble',
-      data: {
-          datasets: [{
-            label: 'Bubble Dataset',
-            data: [
-              {
-                x: 10,
-                y: 20,
-                r: 30
-              },
-              {
-                x: 15,
-                y: 25,
-                r: 35
-              }
-            ]
-            }]
-          },
-      options: {
-          scales: {
-              xAxes: [{
-                  type: 'linear',
-                  position: 'bottom'
-              }]
-          }
-      }
-  });
-};
-
-
-});
+// $(document).ready(function(){
+//
+//   $.ajax({
+//     type: 'GET',
+//     contentType: 'application/json; charset=utf-8',
+//     url: '../graphs/bubble',
+//     dataType: 'json',
+//     success: function(data){
+//       drawBubble(data);
+//       },
+//     failure: function(result){
+//       error();
+//       }
+//   });
+//
+// function error() {
+//     console.log("Something went wrong!");
+// }
+//
+// function drawBubble(data) {
+//
+//   var ctx = document.getElementById("bubbleGraph");
+//   console.log(data);
+//
+//
+//   var bubble = new Chart(ctx, {
+//       type: 'bubble',
+//       data: {
+//           datasets: [{
+//             label: 'Bubble Dataset',
+//             data: [
+//               {
+//                 x: 10,
+//                 y: 20,
+//                 r: 30
+//               },
+//               {
+//                 x: 15,
+//                 y: 25,
+//                 r: 35
+//               }
+//             ]
+//             }]
+//           },
+//       options: {
+//           scales: {
+//               xAxes: [{
+//                   type: 'linear',
+//                   position: 'bottom'
+//               }]
+//           }
+//       }
+//   });
+// };
+//
+//
+// });
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,18 +2,32 @@ $(document).ready(function(){
 
   const navButton = document.getElementById('nav-button');
   const closeNvg = document.getElementById('close-nav');
+  const sideNav = document.getElementById('sideNav');
+  const mainArea = document.getElementById('mainArea');
+
   const newButton = document.getElementById('new-button');
   const closeModal = document.getElementById('close-new');
   const newModal = document.getElementById('new-modal');
 
+  function toggleNav() {
+    if (sideNav.style.width === "0px" || sideNav.style.width === "") {
+      sideNav.style.width = "250px";
+      mainArea.style.marginLeft = "250px";
+    }
+    else if (sideNav.style.width === "250px") {
+      sideNav.style.width = "0";
+      mainArea.style.marginLeft = "0";
+    };
+  };
+
   function openNav() {
-      document.getElementById("sideNav").style.width = "250px";
-      document.getElementById("mainArea").style.marginLeft = "250px";
+      sideNav.style.width = "250px";
+      mainArea.style.marginLeft = "250px";
   };
 
   function closeNav() {
-      document.getElementById("sideNav").style.width = "0";
-      document.getElementById("mainArea").style.marginLeft = "0";
+      sideNav.style.width = "0";
+      mainArea.style.marginLeft = "0";
   };
 
   function openNew() {
@@ -24,7 +38,7 @@ $(document).ready(function(){
       newModal.classList.add('hidden');
   };
 
-  navButton.addEventListener('click', openNav);
+  navButton.addEventListener('click', toggleNav);
   closeNvg.addEventListener('click', closeNav);
   newButton.addEventListener('click', openNew);
   closeModal.addEventListener('click', closeNew);
