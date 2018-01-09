6538d10ec18acda50ecbad0af996d56e7566de8b
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-09 12:05:37 -0500

    small fixes

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,46 +1,29 @@
 $(document).ready(function(){
 
   const navButton = document.getElementById('nav-button');
-  const closeNvg = document.getElementById('close-nav');
   const sideNav = document.getElementById('sideNav');
-  const mainArea = document.getElementById('mainArea');
 
   const newButton = document.getElementById('new-button');
-  const closeModal = document.getElementById('close-new');
   const newModal = document.getElementById('new-modal');
 
   function toggleNav() {
     if (sideNav.style.width === "0px" || sideNav.style.width === "") {
-      sideNav.style.width = "250px";
-      mainArea.style.marginLeft = "250px";
-    }
-    else if (sideNav.style.width === "250px") {
+      sideNav.style.width = "200px";
+    } else if (sideNav.style.width === "200px") {
       sideNav.style.width = "0";
-      mainArea.style.marginLeft = "0";
+      };
+  };
+
+  function toggleNew() {
+    if (newModal.style.height === "0px" || newModal.style.height === ""){
+      newModal.style.height = "100px";
+    } else if (newModal.style.height === "100px"){
+      newModal.style.height = "0px";
     };
   };
 
-  function openNav() {
-      sideNav.style.width = "250px";
-      mainArea.style.marginLeft = "250px";
-  };
-
-  function closeNav() {
-      sideNav.style.width = "0";
-      mainArea.style.marginLeft = "0";
-  };
-
-  function openNew() {
-      newModal.classList.remove('hidden');
-  };
-
-  function closeNew() {
-      newModal.classList.add('hidden');
-  };
 
   navButton.addEventListener('click', toggleNav);
-  closeNvg.addEventListener('click', closeNav);
-  newButton.addEventListener('click', openNew);
-  closeModal.addEventListener('click', closeNew);
+  newButton.addEventListener('click', toggleNew);
 
 });
