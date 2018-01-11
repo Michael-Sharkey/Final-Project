a4fc9aa6ef54c87b9f2526ed8e91065dc6ad9089
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-11 17:35:12 -0500

    basic meal slideshow layout

--- app/assets/javascripts/meals.js
+++ app/assets/javascripts/meals.js
@@ -1,4 +1,34 @@
-// $(document).ready(function(){
+$(document).on('turbolinks:load', function() {
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 //   var photos = document.getElementsByClassName('carousel-inner');
 //   var lastMeal = photos[0].firstElementChild;
 //
@@ -7,4 +37,4 @@
 //     console.log(lastMeal.classList);
 //   });
 //
-// })
+})
