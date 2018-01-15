d43064ba3847116860e4917bdef1faa4058c6459
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-14 19:00:24 -0500

    add accordion js functionality to note list

---
+++ app/assets/javascripts/journals.js
@@ -0,0 +1,18 @@
+$(document).on('turbolinks:load', function() {
+
+
+  var titles = document.getElementsByClassName('note-title');
+  var entries = document.getElementsByClassName('note-contents');
+  var parent = document.getElementById('notes-accordion');
+
+  function toggleView(event) {
+    if (event.target && event.target.classList.contains('note-title')) {
+      event.target.nextElementSibling.classList.toggle('hidden');
+    };
+  };
+
+  parent.addEventListener('click', function(){
+    toggleView(event);
+  });
+
+})
